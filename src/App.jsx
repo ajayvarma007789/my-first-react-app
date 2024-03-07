f2bde69f46c37c4bb36528f7d86132d8a68f7946
ajayvarma <vaajay322@gmail.com>
2024-03-07 20:41:47 +0530

    first-time

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+
+function App() {
+  return(
+    <>
+      <header/>
+      <footer/>
+    </>
+  );
+}
+
+export default App
